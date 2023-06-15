bbbd391dffa258633320251f36fb21562f4b1af5
Miraculixx <miraculixxyt@gmail.com>
2023-06-15 02:06:45 +0200

    Allowing custom data folders

--- src/main/kotlin/net/axay/kspigot/config/PluginFile.kt
+++
@@ -1,9 +0,0 @@
-package net.axay.kspigot.config
-
-import net.axay.kspigot.main.PluginInstance
-import java.io.File
-
-class PluginFile(path: String, child: String? = null) : File(
-    "${PluginInstance.dataFolder}",
-    if (child == null) path else File(path, child).path
-)
