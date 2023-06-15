bf6809c72a26b7c8fe2e892514adee4ed9d071e7
Miraculixx <miraculixxyt@gmail.com>
2023-06-15 02:00:55 +0200

    Unnecessary + Paper replacement
    Function can be skipped by papers raycast method to find target entity or block in range

--- src/main/kotlin/net/axay/kspigot/extensions/events/InteractEventExtensions.kt
+++ src/main/kotlin/net/axay/kspigot/extensions/events/InteractEventExtensions.kt
@@ -1,12 +1,10 @@
+@file:Suppress("unused")
+
 package net.axay.kspigot.extensions.events
 
-import net.axay.kspigot.annotations.UnsafeImplementation
-import org.bukkit.block.Block
-import org.bukkit.entity.Entity
 import org.bukkit.entity.Player
 import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEntityEvent
-import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.EquipmentSlot
 import org.bukkit.inventory.ItemStack
 
@@ -34,18 +32,3 @@ val Action.isLeftClick get() = this == Action.LEFT_CLICK_BLOCK || this == Action
  * @return True, if the action was a right mouse button click.
  */
 val Action.isRightClick get() = this == Action.RIGHT_CLICK_BLOCK || this == Action.RIGHT_CLICK_AIR
-
-@UnsafeImplementation
-val PlayerInteractEvent.clickedBlockExceptAir: Block?
-    get() {
-        return clickedBlock ?: kotlin.run {
-            return@run if (this.action == Action.RIGHT_CLICK_AIR) {
-                val p: Player = this.player
-                // check for sight blocking entities
-                for (nearbyEntity: Entity in p.getNearbyEntities(5.0, 5.0, 5.0))
-                    if (p.hasLineOfSight(nearbyEntity)) return@run null
-                // get first block in line of sight which is not air
-                p.getLineOfSight(null, 5).find { block -> !block.type.isAir }
-            } else null
-        }
-    }
