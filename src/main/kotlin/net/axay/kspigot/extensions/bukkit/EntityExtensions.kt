eede4c7ede1d9bfb3f7196f4515edca0bba0bb25
Miraculixx <miraculixxyt@gmail.com>
2023-06-15 02:04:08 +0200

    Simplify + renaming
    Simplify methods by merging them and renaming visibility functions to match internal naming. Ground blocks can be replaced with 1.20 new system

--- src/main/kotlin/net/axay/kspigot/extensions/bukkit/EntityExtensions.kt
+++ src/main/kotlin/net/axay/kspigot/extensions/bukkit/EntityExtensions.kt
@@ -2,25 +2,14 @@
 
 package net.axay.kspigot.extensions.bukkit
 
-import net.axay.kspigot.annotations.NMS_General
-import net.axay.kspigot.chat.literalText
 import net.axay.kspigot.extensions.onlinePlayers
 import net.axay.kspigot.main.PluginInstance
 import net.axay.kspigot.pluginmessages.PluginMessageConnect
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.title.Title
-import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.attribute.Attribute
-import org.bukkit.entity.ArmorStand
-import org.bukkit.entity.Damageable
-import org.bukkit.entity.Entity
-import org.bukkit.entity.EntityType
-import org.bukkit.entity.LivingEntity
-import org.bukkit.entity.Player
+import org.bukkit.entity.*
 import org.bukkit.inventory.EquipmentSlot
 import org.bukkit.inventory.ItemStack
-import java.time.Duration
 
 /**
  * Checks if the entities' head is in water.
@@ -31,11 +20,6 @@ val LivingEntity.isHeadInWater: Boolean get() = this.eyeLocation.block.type == M
  * Checks if the entities' feet are in water.
  */
 val Entity.isFeetInWater: Boolean get() = this.location.block.type == Material.WATER
-
-/**
- * Checks if the entity stands on solid ground.
- */
-val Entity.isGroundSolid: Boolean get() = this.location.add(0.0, -0.01, 0.0).block.type.isSolid
 
 /**
  * Returns the material that is present under the feet of this entity.
@@ -51,24 +35,17 @@ val Entity.isStandingOnBlock: Boolean
     get() = groundMaterial.isSolid
 
 /**
- * @returns true if the entity is standing in mid air.
+ * @return true if the entity is standing in mid air.
  */
 val Entity.isStandingInMidAir: Boolean
     get() = !isStandingOnBlock && vehicle == null && !location.clone().add(0.0, 0.1, 0.0).block.type.isSolid && !location.block.type.isSolid
 
 /**
- * @return The max health of the entity
- * @throws NullPointerException if the entity doesn't have a max health value
- */
-val LivingEntity.realMaxHealth: Double
-    get() = getAttribute(Attribute.GENERIC_MAX_HEALTH)?.value
-        ?: throw NullPointerException("The entity does not have a max health value!")
-
-/**
- * Kills the damageable.
+ * Kills the damageable with a hit animation
  */
 fun Damageable.kill() {
-    health = 0.0
+    damage(0.1)
+    if (health > 0.0) health = 0.0
 }
 
 /**
@@ -81,25 +58,9 @@ fun LivingEntity.heal() {
 }
 
 /**
- * Sets the players' foodLevel to the
- * max possible value.
+ * Sets the players' foodLevel and saturation to the max possible value.
  */
 fun Player.feed() {
-    foodLevel = 20
-}
-
-/**
- * Sets the players' saturation to the
- * current max possible value.
- */
-fun Player.saturate() {
-    saturation = foodLevel.toFloat()
-}
-
-/**
- * Feeds and saturates the player.
- */
-fun Player.feedSaturate() {
     foodLevel = 20
     saturation = 20f
 }
@@ -107,14 +68,14 @@ fun Player.feedSaturate() {
 /**
  * Hides the player for all [onlinePlayers].
  */
-fun Player.disappear() {
+fun Player.hideSelf() {
     onlinePlayers.filter { it != this }.forEach { it.hidePlayer(PluginInstance, this) }
 }
 
 /**
  * Shows the player for all [onlinePlayers].
  */
-fun Player.appear() {
+fun Player.showSelf() {
     onlinePlayers.filter { it != this }.forEach { it.showPlayer(PluginInstance, this) }
 }
 
@@ -133,50 +94,20 @@ fun Player.showOnlinePlayers() {
 }
 
 /**
- * Spawns an entity without any variations in color, type etc...
+ * Hide everyone from everyone (nobody sees another player)
  */
-@Deprecated("This function is unstable and it cannot be guaranteed that it will work at any time in the future.")
-@NMS_General
-fun Location.spawnCleanEntity(entityType: EntityType): Entity? {
-    val craftWorld = world as? org.bukkit.craftbukkit.v1_20_R1.CraftWorld ?: return null
-    return craftWorld.createEntity(this, entityType.entityClass)?.let {
-        craftWorld.handle.addFreshEntity(it)
-        return@let it.bukkitEntity
-    }
-}
-
-/**
- * @param mainText title text
- * @param subText subtitle text
- * @param fadeIn time in ticks for titles to fade in
- * @param stay time in ticks for titles to stay
- * @param fadeOut time in ticks for titles to fade out
- */
-fun Player.title(
-    mainText: Component = Component.empty(),
-    subText: Component = Component.empty(),
-    fadeIn: Duration = Duration.ofMillis(500),
-    stay: Duration = Duration.ofMillis(3500),
-    fadeOut: Duration = Duration.ofMillis(1000),
-) {
-    showTitle(Title.title(mainText, subText, Title.Times.times(fadeIn, stay, fadeOut)))
+fun hideEveryone() {
+    onlinePlayers.forEach { it.hideOnlinePlayers() }
 }
 
 /**
  * Returns the itemInHand of the given [EquipmentSlot]
  * if it is an hand slot.
  */
-fun Player.getHandItem(hand: EquipmentSlot?) = when (hand) {
+fun Player.getHandItem(hand: EquipmentSlot) = when (hand) {
     EquipmentSlot.HAND -> inventory.itemInMainHand
     EquipmentSlot.OFF_HAND -> inventory.itemInOffHand
     else -> null
-}
-
-/**
- * Sends the given [text] as an action bar message.
- */
-fun Player.actionBar(text: String) {
-    sendActionBar(literalText { legacyText(text) })
 }
 
 /**
@@ -198,16 +129,19 @@ fun Player.give(vararg itemStacks: ItemStack) = inventory.addItem(*itemStacks)
  */
 fun ArmorStand.fullLock() {
     for (slot in EquipmentSlot.values()) {
-        lock(slot)
+        for (lock in ArmorStand.LockType.values()) {
+            addEquipmentLock(slot, lock)
+        }
     }
 }
 
 /**
- * Adds all equipment locks to the given slot
- * @param slot the slot which gets locked
+ * Removes all equipment locks from every equipment slot
  */
-fun ArmorStand.lock(slot: EquipmentSlot) {
-    for (lock in ArmorStand.LockType.values()) {
-        addEquipmentLock(slot, lock)
+fun ArmorStand.fullUnlock() {
+    for (slot in EquipmentSlot.values()) {
+        for (lock in ArmorStand.LockType.values()) {
+            removeEquipmentLock(slot, lock)
+        }
     }
 }
